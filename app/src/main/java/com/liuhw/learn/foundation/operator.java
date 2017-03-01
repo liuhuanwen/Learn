fc55c6029f7d2861a89b3046823eb618140ab4aa
huanwen <liuhuanwen@zhidekan.me>
2017-03-01 09:54:20 +0800

    位运算符测试

---
+++ app/src/main/java/com/liuhw/learn/foundation/operator.java
@@ -0,0 +1,24 @@
+package com.liuhw.learn.foundation;
+
+/**
+ * Created by liuhw on 2017/2/28.
+ * 位运算符操作
+ */
+
+public class operator {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    /*位运算符*/
+    private static void test1() {
+        int a = 123;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(a >> 1);
+        int b = ~a;
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(b);
+        System.out.println(b >> 1);
+    }
+}
